7062c4b2705fc52047c18eae6b4091c61e770abd
Kinga Kazala <22429087+kkazala@users.noreply.github.com>
2023-02-01 14:31:02 +0100

    tbc

--- src/extensions/panel/PanelCommandSet.ts
+++ src/extensions/panel/PanelCommandSet.ts
@@ -1,11 +1,6 @@
 import { AnalyticsPlugin } from "@microsoft/applicationinsights-analytics-js";
 import { override } from "@microsoft/decorators";
-import {
-  BaseListViewCommandSet,
-  Command,
-  IListViewCommandSetExecuteEventParameters,
-  ListViewStateChangedEventArgs,
-} from "@microsoft/sp-listview-extensibility";
+import { BaseListViewCommandSet, Command, IListViewCommandSetExecuteEventParameters, ListViewStateChangedEventArgs } from "@microsoft/sp-listview-extensibility";
 import { ConsoleListener, Logger } from "@pnp/logging";
 import { SPFI, spfi, SPFx } from "@pnp/sp";
 import * as strings from "PanelCommandSetStrings";
@@ -19,222 +14,202 @@ import { IStatefulPanelProps } from "./components/StatefulPanel/IStatefulPanelPr
 import StatefulPanel from "./components/StatefulPanel/StatefulPanel";
 
 export interface IPanelCommandSetProperties {
-  logLevel?: number;
-  listName: string;
-  appInsightsConnString?: string;
+	logLevel?: number;
+	listName: string;
+	appInsightsConnString?: string;
 }
 interface IProcessConfigResult {
-  visible: boolean;
-  disabled: boolean;
-  title: string;
+	visible: boolean;
+	disabled: boolean;
+	title: string;
 }
 
 const LOG_SOURCE: string = "PanelCommandSet";
 
 export default class PanelCommandSet extends BaseListViewCommandSet<IPanelCommandSetProperties> {
-  //#region variables
-  private panelPlaceHolder: HTMLDivElement = null;
-  private panelTop: number;
-  private panelId: string;
-  private compId: string;
-  private spfiContext: SPFI;
-  private appInsights: AnalyticsPlugin;
-  //#endregion
+	//#region variables
+	private panelPlaceHolder: HTMLDivElement = null;
+	private panelTop: number;
+	private panelId: string;
+	private compId: string;
+	private spfiContext: SPFI;
+	private appInsights: AnalyticsPlugin;
+	//#endregion
 
-  @override
-  public onInit(): Promise<void> {
-    //list name registered in Extension properties
-    const _isListRegistered =
-      this.context.listView.list.title === this.properties.listName
-        ? true
-        : false;
+	@override
+	public onInit(): Promise<void> {
+		//list name registered in Extension properties
+		const _isListRegistered = this.context.listView.list.title === this.properties.listName ? true : false;
 
-    const _setLogger = (appInsights?: AnalyticsPlugin): void => {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      Logger.subscribe(new (ConsoleListener as any)());
-      if (appInsights !== undefined) {
-        Logger.subscribe(new AppInsightsLogListener(appInsights));
-      }
+		const _setLogger = (appInsights?: AnalyticsPlugin): void => {
+			// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			Logger.subscribe(new (ConsoleListener as any)());
+			if (appInsights !== undefined) {
+				Logger.subscribe(new AppInsightsLogListener(appInsights));
+			}
 
-      if (
-        this.properties.logLevel &&
-        this.properties.logLevel in [0, 1, 2, 3, 99]
-      ) {
-        Logger.activeLogLevel = this.properties.logLevel;
-      }
+			if (this.properties.logLevel && this.properties.logLevel in [0, 1, 2, 3, 99]) {
+				Logger.activeLogLevel = this.properties.logLevel;
+			}
 
-      Logger.write(`${LOG_SOURCE} Activated Initialized with properties:`);
-      Logger.write(
-        `${LOG_SOURCE} ${JSON.stringify(this.properties, undefined, 2)}`
-      );
-    };
-    const _setPanel = (): void => {
-      this.panelTop = document.querySelector("#SuiteNavWrapper").clientHeight;
-      this.panelPlaceHolder = document.body.appendChild(
-        document.createElement("div")
-      );
-    };
-    const _setCommands = (): void => {
-      const _setCommandState = (
-        command: Command,
-        config: IProcessConfigResult
-      ): void => {
-        command.visible = config.visible;
-        command.disabled = config.disabled;
-        command.title = config.title;
-      };
+			Logger.write(`${LOG_SOURCE} Activated Initialized with properties:`);
+			Logger.write(`${LOG_SOURCE} ${JSON.stringify(this.properties, undefined, 2)}`);
+		};
+		const _setPanel = (): void => {
+			this.panelTop = document.querySelector("#SuiteNavWrapper").clientHeight;
+			this.panelPlaceHolder = document.body.appendChild(document.createElement("div"));
+		};
+		const _setCommands = (): void => {
+			const _setCommandState = (command: Command, config: IProcessConfigResult): void => {
+				command.visible = config.visible;
+				command.disabled = config.disabled;
+				command.title = config.title;
+			};
 
-      //Command 1 always visible and enabled in 'Travel Requests' list
-      const compareOneCommand: Command = this.tryGetCommand("COMMAND_1");
-      if (compareOneCommand) {
-        _setCommandState(compareOneCommand, {
-          title: strings.Command1,
-          visible: true,
-          disabled: false,
-        });
-      }
-      //Command 2 always visible in 'Travel Requests' list, enabled if selectedItem
-      const compareTwoCommand: Command = this.tryGetCommand("COMMAND_2");
-      if (compareTwoCommand) {
-        _setCommandState(compareTwoCommand, {
-          title: strings.Command2,
-          visible: true,
-          disabled: true,
-        });
-      }
+			//Command 1 always visible and enabled in 'Travel Requests' list
+			const compareOneCommand: Command = this.tryGetCommand("COMMAND_1");
+			if (compareOneCommand) {
+				_setCommandState(compareOneCommand, {
+					title: strings.Command1,
+					visible: true,
+					disabled: false,
+				});
+			}
+			//Command 2 always visible in 'Travel Requests' list, enabled if selectedItem
+			const compareTwoCommand: Command = this.tryGetCommand("COMMAND_2");
+			if (compareTwoCommand) {
+				_setCommandState(compareTwoCommand, {
+					title: strings.Command2,
+					visible: true,
+					disabled: true,
+				});
+			}
 
-      this.raiseOnChange();
-    };
+			this.raiseOnChange();
+		};
 
-    if (!_isListRegistered) {
-      return;
-    }
+		if (!_isListRegistered) {
+			return;
+		}
 
-    if (this.properties.appInsightsConnString) {
-      this.appInsights = AppInsights(this.properties.appInsightsConnString);
-      // this.appInsights.trackPageView(); //DON'T, this will record page view twice
-      _setLogger(this.appInsights);
-    } else {
-      _setLogger();
-    }
+		if (this.properties.appInsightsConnString) {
+			this.appInsights = AppInsights(this.properties.appInsightsConnString);
+			// this.appInsights.trackPageView(); //DON'T, this will record page view twice
+			_setLogger(this.appInsights);
+		} else {
+			_setLogger();
+		}
 
-    _setPanel();
-    _setCommands();
+		_setPanel();
+		_setCommands();
 
-    //#region IMPORTANT!
-    // To avoid timeout after 30 minutes: "The security validation for this page is invalid and might be corrupted
-    // This issue only occurs when the spfi() object and spfx behavior is instantiated multiple times during the lifecycle of a web part.
-    // See: https://github.com/pnp/pnpjs/issues/2304
-    //#endregion
-    this.spfiContext = spfi().using(SPFx(this.context));
-    this.context.listView.listViewStateChangedEvent.add(
-      this,
-      this._onListViewStateChanged
-    );
+		//#region IMPORTANT!
+		// To avoid timeout after 30 minutes: "The security validation for this page is invalid and might be corrupted
+		// This issue only occurs when the spfi() object and spfx behavior is instantiated multiple times during the lifecycle of a web part.
+		// See: https://github.com/pnp/pnpjs/issues/2304
+		//#endregion
+		this.spfiContext = spfi().using(SPFx(this.context));
+		this.context.listView.listViewStateChangedEvent.add(this, this._onListViewStateChanged);
 
-    return Promise.resolve();
-  }
+		return Promise.resolve();
+	}
 
-  // Triggered when row(s) un/selected
-  public _onListViewStateChanged(args: ListViewStateChangedEventArgs): void {
-    const itemSelected =
-      this.context.listView.selectedRows &&
-      this.context.listView.selectedRows.length === 1;
+	// Triggered when row(s) un/selected
+	public _onListViewStateChanged(args: ListViewStateChangedEventArgs): void {
+		const itemSelected = this.context.listView.selectedRows && this.context.listView.selectedRows.length === 1;
 
-    const compareTwoCommand: Command = this.tryGetCommand("COMMAND_2");
-    if (compareTwoCommand && compareTwoCommand.disabled === itemSelected) {
-      compareTwoCommand.disabled = !itemSelected;
-      this.raiseOnChange();
-    }
+		const compareTwoCommand: Command = this.tryGetCommand("COMMAND_2");
+		if (compareTwoCommand && compareTwoCommand.disabled === itemSelected) {
+			compareTwoCommand.disabled = !itemSelected;
+			this.raiseOnChange();
+		}
 
-    //#region NOTE
-    // NOTE: use raiseOnChange() with caution; frequent calls can lead to low performance of the list
-    // https://github.com/SharePoint/sp-dev-docs/discussions/7375#discussioncomment-2053604
-    //#endregion
-  }
+		//#region NOTE
+		// NOTE: use raiseOnChange() with caution; frequent calls can lead to low performance of the list
+		// https://github.com/SharePoint/sp-dev-docs/discussions/7375#discussioncomment-2053604
+		//#endregion
+	}
 
-  @override
-  public onExecute(event: IListViewCommandSetExecuteEventParameters): void {
-    const _showComponent = (props: IMyComponentProps): void => {
-      //#region comments
-      //using item's ID as a key will ensure the form is re-rendered when selection is changed
-      //BUT if Panel for the Item has been opened (rendered), and
-      //   - panel is closed
-      //   - sb changed item properties (another browser)
-      //   - wait for list to refresh and show the new property
-      //   - open Panel: old value becasue panel is not re-rendered
-      // const ChangeToken = props.selectedRows[0].getValueByName("ID");
-      //#endregion
+	@override
+	public onExecute(event: IListViewCommandSetExecuteEventParameters): void {
+		const _showComponent = (props: IMyComponentProps): void => {
+			//#region comments
+			//using item's ID as a key will ensure the form is re-rendered when selection is changed
+			//BUT if Panel for the Item has been opened (rendered), and
+			//   - panel is closed
+			//   - sb changed item properties (another browser)
+			//   - wait for list to refresh and show the new property
+			//   - open Panel: old value becasue panel is not re-rendered
+			// const ChangeToken = props.selectedRows[0].getValueByName("ID");
+			//#endregion
 
-      this.compId = Date.now().toString();
-      this.panelPlaceHolder.setAttribute("id", this.compId);
-      const element: React.ReactElement<IMyComponentProps> =
-        React.createElement(MyComponent, {
-          ...props,
-          key: this.compId,
-        });
+			this.compId = Date.now().toString();
+			this.panelPlaceHolder.setAttribute("id", this.compId);
+			const element: React.ReactElement<IMyComponentProps> = React.createElement(MyComponent, {
+				...props,
+				key: this.compId,
+			});
 
-      ReactDOM.render(element, this.panelPlaceHolder);
-    };
-    const _showPanel = (props: IStatefulPanelProps): void => {
-      this.panelId = Date.now().toString();
-      this.panelPlaceHolder.setAttribute("id", this.panelId);
+			ReactDOM.render(element, this.panelPlaceHolder);
+		};
+		const _showPanel = (props: IStatefulPanelProps): void => {
+			this.panelId = Date.now().toString();
+			this.panelPlaceHolder.setAttribute("id", this.panelId);
 
-      const element: React.ReactElement<IStatefulPanelProps> =
-        React.createElement(
-          StatefulPanel,
-          {
-            ...props,
-            key: this.panelId,
-          },
-          React.createElement("div", {
-            dangerouslySetInnerHTML: { __html: strings.htmlInfo },
-          })
-        );
+			const element: React.ReactElement<IStatefulPanelProps> = React.createElement(
+				StatefulPanel,
+				{
+					...props,
+					key: this.panelId,
+				},
+				React.createElement("div", {
+					dangerouslySetInnerHTML: { __html: strings.htmlInfo },
+				})
+			);
 
-      ReactDOM.render(element, this.panelPlaceHolder);
-    };
-    const _refreshList = (): void => {
-      Logger.write(strings.lblRefreshing);
-      location.reload();
-    };
-    const _onCompleted = (success: boolean): void => {
-      if (this.appInsights !== undefined) {
-        this.appInsights.trackEvent({
-          name: success ? strings.lblItemUpdate_OK : strings.lblItemUpdate_Err,
-        });
-      }
-    };
+			ReactDOM.render(element, this.panelPlaceHolder);
+		};
+		const _refreshList = (): void => {
+			Logger.write(strings.lblRefreshing);
+			location.reload();
+		};
+		const _onCompleted = (success: boolean): void => {
+			if (this.appInsights !== undefined) {
+				this.appInsights.trackEvent({
+					name: success ? strings.lblItemUpdate_OK : strings.lblItemUpdate_Err,
+				});
+			}
+		};
 
-    switch (event.itemId) {
-      case "COMMAND_1":
-        _showPanel({
-          shouldOpen: true,
-          title: strings.titleTravelGuidelines,
-          panelTop: this.panelTop,
-        });
-        break;
-      case "COMMAND_2":
-        _showComponent({
-          panelConfig: {
-            panelTop: this.panelTop,
-            shouldOpen: true,
-            title: strings.titleTravelReport,
-            onDismiss: _refreshList,
-          },
-          spfiContext: this.spfiContext,
-          listName: this.context.listView.list.title,
-          selectedRows: event.selectedRows,
-          onCompleted: _onCompleted,
-        });
-        break;
-      default:
-        throw new Error("Unknown command");
-    }
-  }
+		switch (event.itemId) {
+			case "COMMAND_1":
+				_showPanel({
+					shouldOpen: true,
+					title: strings.titleTravelGuidelines,
+					panelTop: this.panelTop,
+				});
+				break;
+			case "COMMAND_2":
+				_showComponent({
+					panelConfig: {
+						panelTop: this.panelTop,
+						shouldOpen: true,
+						title: strings.titleTravelReport,
+						onDismiss: _refreshList,
+					},
+					spfiContext: this.spfiContext,
+					listName: this.context.listView.list.title,
+					selectedRows: event.selectedRows,
+					onCompleted: _onCompleted,
+				});
+				break;
+			default:
+				throw new Error("Unknown command");
+		}
+	}
 
-  public onDispose(): void {
-    ReactDOM.unmountComponentAtNode(document.getElementById(this.panelId));
-    ReactDOM.unmountComponentAtNode(document.getElementById(this.componentId));
-  }
+	public onDispose(): void {
+		ReactDOM.unmountComponentAtNode(document.getElementById(this.panelId));
+		ReactDOM.unmountComponentAtNode(document.getElementById(this.componentId));
+	}
 }
--- src/extensions/panel/components/StatefulPanel/StatefulPanel.tsx
+++ src/extensions/panel/components/StatefulPanel/StatefulPanel.tsx
@@ -2,49 +2,92 @@ import { IPanelStyles, Panel, PanelType } from "@fluentui/react";
 import { useBoolean } from "@fluentui/react-hooks";
 import { loadStyles } from "@microsoft/load-themed-styles";
 import * as React from "react";
+import ReactDOM from "react-dom";
 import { IStatefulPanelProps } from "./IStatefulPanelProps";
 
-export default function StatefulPanel(
-  props: React.PropsWithChildren<IStatefulPanelProps>
-): JSX.Element {
-  const IframePanelStyles: Partial<IPanelStyles> = {
-    root: { top: props.panelTop },
-  };
-  const [isOpen, { setTrue: setPanelOpen, setFalse: setPanelClosed }] =
-    useBoolean(false);
+export default function StatefulPanel(props: React.PropsWithChildren<IStatefulPanelProps>): JSX.Element {
+	const IframePanelStyles: Partial<IPanelStyles> = {
+		root: { top: props.panelTop },
+	};
+	const [isOpen, { setTrue: setPanelOpen, setFalse: setPanelClosed }] = useBoolean(false);
 
-  React.useEffect(() => {
-    loadStyles("panel");
-  }, []);
+	React.useEffect(() => {
+		loadStyles("panel");
+		setPanelOpen();
+	}, []);
 
-  React.useEffect(() => {
-    if (props.shouldOpen && !isOpen) {
-      setPanelOpen();
-    }
-  }, [props.shouldOpen]);
+	// React.useEffect(() => {
+	// 	if (props.shouldOpen && !isOpen) {
+	// 		setPanelOpen();
+	// 	}
+	// }, [props.shouldOpen]);
 
-  const _onPanelClosed = (): void => {
-    props.shouldOpen = false;
-    setPanelClosed();
+	const _onPanelClosed = (): void => {
+		// props.shouldOpen = false;
+		setPanelClosed();
+		// unmountComponentAtNode(this.dom)
+		ReactDOM.unmountComponentAtNode(document.getElementById(this.key));
 
-    if (props.onDismiss !== undefined) {
-      props.onDismiss();
-    }
-  };
+		if (props.onDismiss !== undefined) {
+			props.onDismiss();
+		}
+	};
 
-  return (
-    <Panel
-      className="od-Panel"
-      headerText={props.title}
-      isOpen={isOpen}
-      type={PanelType.medium}
-      isLightDismiss={false}
-      styles={IframePanelStyles}
-      // key={ props.uniqueKey}
-      onDismiss={_onPanelClosed}
-    >
-      {/* Ensure there are children to render, otherwise ErrorBoundary throws error */}
-      {props.children && <>{props.children}</>}
-    </Panel>
-  );
+	return (
+		<Panel
+			className="od-Panel"
+			headerText={props.title}
+			isOpen={isOpen}
+			type={PanelType.medium}
+			isLightDismiss={false}
+			styles={IframePanelStyles}
+			// key={ props.uniqueKey}
+			onDismiss={_onPanelClosed}
+		>
+			{/* Ensure there are children to render, otherwise ErrorBoundary throws error */}
+			{props.children && <>{props.children}</>}
+		</Panel>
+	);
 }
+
+// export default function StatefulPanel(props: React.PropsWithChildren<IStatefulPanelProps>): JSX.Element {
+// 	const IframePanelStyles: Partial<IPanelStyles> = {
+// 		root: { top: props.panelTop },
+// 	};
+// 	const [isOpen, { setTrue: setPanelOpen, setFalse: setPanelClosed }] = useBoolean(false);
+
+// 	React.useEffect(() => {
+// 		loadStyles("panel");
+// 	}, []);
+
+// 	React.useEffect(() => {
+// 		if (props.shouldOpen && !isOpen) {
+// 			setPanelOpen();
+// 		}
+// 	}, [props.shouldOpen]);
+
+// 	const _onPanelClosed = (): void => {
+// 		props.shouldOpen = false;
+// 		setPanelClosed();
+
+// 		if (props.onDismiss !== undefined) {
+// 			props.onDismiss();
+// 		}
+// 	};
+
+// 	return (
+// 		<Panel
+// 			className="od-Panel"
+// 			headerText={props.title}
+// 			isOpen={isOpen}
+// 			type={PanelType.medium}
+// 			isLightDismiss={false}
+// 			styles={IframePanelStyles}
+// 			// key={ props.uniqueKey}
+// 			onDismiss={_onPanelClosed}
+// 		>
+// 			{/* Ensure there are children to render, otherwise ErrorBoundary throws error */}
+// 			{props.children && <>{props.children}</>}
+// 		</Panel>
+// 	);
+// }
